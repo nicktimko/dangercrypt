804d3234fd5d5aab2294f49a12bd05b847e51d5a
Nick Timkovich <nick@telnyx.com>
2021-05-05 08:46:02 -0500

    More finite field math

--- finite.py
+++ finite.py
@@ -22,30 +22,123 @@ def get_prime_factors(number):
     return prime_factors
 
 
+UNICODE_SUPERSCRIPTS = {
+    ord("0"): "\N{SUPERSCRIPT ZERO}",
+    ord("1"): "\N{SUPERSCRIPT ONE}",
+    ord("2"): "\N{SUPERSCRIPT TWO}",
+    ord("3"): "\N{SUPERSCRIPT THREE}",
+    ord("4"): "\N{SUPERSCRIPT FOUR}",
+    ord("5"): "\N{SUPERSCRIPT FIVE}",
+    ord("6"): "\N{SUPERSCRIPT SIX}",
+    ord("7"): "\N{SUPERSCRIPT SEVEN}",
+    ord("8"): "\N{SUPERSCRIPT EIGHT}",
+    ord("9"): "\N{SUPERSCRIPT NINE}",
+    ord("+"): "\N{SUPERSCRIPT PLUS SIGN}",
+    ord("-"): "\N{SUPERSCRIPT MINUS}",
+}
+
+
+def int2sup(n: int) -> str:
+    """
+    Integer to superscripts
+    """
+    return str(n).translate(UNICODE_SUPERSCRIPTS)
+
+
+POLY_MIN_WEIGHT = {
+    # Hansen and Mullen. Supplement to Primitive Polynomials over Finite Fields
+    # https://www.ams.org/journals/mcom/1992-59-200/S0025-5718-1992-1134730-7/S0025-5718-1992-1134730-7.pdf
+    2: {
+        2: [1, 1, 1],
+        3: [1, 1, 0, 1],
+        4: [1, 1, 0, 0, 1],
+        5: [1, 0, 1, 0, 0, 1],
+        6: [1, 0, 1, 0, 0, 0, 1],
+        7: [1, 1, 0, 0, 0, 0, 0, 1],
+        # 8: [1, 0, 1, 1, 1, 0, 0, 0, 1],  # from paper
+        8: [1, 1, 0, 1, 1, 0, 0, 0, 1],  # AES
+    },
+    3: {
+        2: [2, 1, 1],
+        3: [1, 2, 0, 1],
+        4: [2, 1, 0, 0, 1],
+        5: [1, 2, 0, 0, 0, 1],
+    },
+    5: {
+        2: [2, 1, 2],
+        3: [2, 3, 0, 1],
+        4: [2, 2, 1, 0, 1],
+        5: [2, 4, 0, 0, 0, 1],
+    },
+    7: {
+        2: [3, 1, 1],
+        3: [2, 3, 0, 1],
+        4: [5, 3, 1, 0, 1],
+        5: [4, 1, 0, 0, 0, 1],
+    },
+    11: {
+        2: [7, 1, 1],
+        3: [4, 1, 0, 1],
+    },
+    13: {
+        2: [2, 1, 1],
+        3: [6, 1, 0, 1],
+    },
+    17: {
+        2: [3, 1, 1],
+        3: [3, 1, 0, 1],
+    },
+    19: {
+        2: [2, 1, 1],
+        3: [4, 1, 0, 1],
+    },
+    23: {
+        2: [7, 1, 1],
+        3: [3, 1, 0, 1],
+    },
+}
+
+
 class Field:
-    def __init__(self, n):
+    def __init__(self, n, mod_poly=None):
         self.n = n
+
         factors = collections.Counter(get_prime_factors(n))
         if len(factors) > 1:
             factorization = " * ".join(
-                f"{b}**{p}" if p > 1 else f"{b}" for b, p in factors.items()
+                f"{b}{int2sup(p)}" if p > 1 else f"{b}"
+                for b, p in sorted(factors.items())
             )
             raise ValueError(f"cannot form GF({n}) = GF({factorization})")
 
-        (self.base, self.power), *_ = factors.items()
+        (self.prime, self.power), *_ = factors.items()
+        if self.power == 1:
+            self.mod_poly = [1]
+        else:
+            if mod_poly is None:
+                try:
+                    self.mod_poly = POLY_MIN_WEIGHT[self.prime][self.power]
+                except IndexError:
+                    raise Exception(
+                        f"don't know a primitive polynomial for GF({self.prime}{int2sup(self.power)})"
+                    )
+            else:
+                self.mod_poly = mod_poly
 
     def __repr__(self):
         if self.power == 1:
-            return f"{self.__class__.__name__}({self.base})"
-        return f"{self.__class__.__name__}({self.base}**{self.power})"
+            return f"{self.__class__.__name__}({self.prime})"
+        return f"{self.__class__.__name__}({self.prime}{int2sup(self.power)})"
 
     def __eq__(self, other):
         if not isinstance(other, self.__class__):
             raise TypeError
-        return (self.base, self.power) == (other.base, other.power)
+        return (self.prime, self.power) == (other.prime, other.power)
 
     def element(self, value):
         return Element(self, value)
+
+    E = element
 
 
 class Element:
@@ -55,12 +148,13 @@ class Element:
             coeff = []
             quotient = value
             for _ in range(field.power):
-                quotient, coeff_n = divmod(quotient, field.base)
+                quotient, coeff_n = divmod(quotient, field.prime)
                 coeff.append(coeff_n)
             if quotient:
                 raise ValueError(f"{self.field} can not hold {value}")
         else:
             coeff = value
+            coeff = [c % self.field.prime for c in coeff]
             if len(coeff) != field.power:
                 raise ValueError(
                     f"number of coefficients: {len(coeff)} disagrees with field exponent: {field.power}"
@@ -69,17 +163,120 @@ class Element:
         self.coeff = coeff
 
     def __repr__(self):
-        return f"<{self.__class__.__name__} coeff={self.coeff}>"
+        return f"<{self.__class__.__name__} in {self.field} coeff={self.coeff}>"
 
     def __add__(self, other):
-        new_coeff = [(a + b) % self.field.base for a, b in zip(self.coeff, other.coeff)]
+        new_coeff = [
+            (a + b) % self.field.prime for a, b in zip(self.coeff, other.coeff)
+        ]
         return self.__class__(self.field, new_coeff)
 
-    def __mul__(self, other):
+    def __radd__(self, other):
+        if other == 0:
+            # this enables using sum() on an iterable of Elements
+            return self
+        raise TypeError("unsupported")  # could probably support...
+
+    def __sub__(self, other):
+        new_coeff = [
+            (a - b) % self.field.prime for a, b in zip(self.coeff, other.coeff)
+        ]
+        return self.__class__(self.field, new_coeff)
+
+    def __neg__(self):
+        return self.__class__(self.field, 0) - self
+
+    def __int__(self):
+        return sum(c * self.field.prime ** p for p, c in enumerate(self.coeff))
+
+    def __eq__(self, other):
+        return self.field == other.field and self.coeff == other.coeff
+
+    def __divmod__(self, other):
+        quotient, remainder = poly_divmod(self.coeff, other.coeff)
+
+        # re-pad remainder to same number of coefficients
+        remainder = pad(remainder, self.field.power)
+
+        # the quotient isn't sure to fit in the field, but the remainder will, so
+        # return the former as a generic polynomial, and the remainder as an Element.
+        return quotient, self.__class__(self.field, remainder)
+
+    def __truediv__(self, other):
+        return divmod(self, other)[0]
+
+    def __mod__(self, other):
+        return divmod(self, other)[1]
+
+    def unreduced_mul(self, other):
         result = [0] * (2 * self.field.power - 1)
         for exp_a, coef_a in enumerate(self.coeff):
             for exp_b, coef_b in enumerate(other.coeff):
-                result[exp_a + exp_b] += coef_a + coef_b
-            print(result)
-        result = [n % self.field.base for n in result]
+                result[exp_a + exp_b] += coef_a * coef_b
+        result = [n % self.field.prime for n in result]
         return result
+
+    def __mul__(self, other):
+        raw = self.unreduced_mul(other)
+        if self.field.power > 1:
+            _quotient, remainder = poly_divmod(raw, self.field.mod_poly)
+            remainder = pad(remainder, self.field.power)
+        else:
+            remainder = [raw[0] % self.field.prime]
+        return self.__class__(self.field, remainder)
+
+
+def normalize(poly):
+    while poly and poly[-1] == 0:
+        poly.pop()
+    if poly == []:
+        poly.append(0)
+
+
+def pad(poly, order):
+    return poly + [0] * (order - len(poly))
+
+
+def poly_divmod(num: list[int], den: list[int]) -> tuple[list[int], list[int]]:
+    """
+    Polynomial long division
+
+    From http://stackoverflow.com/questions/26173058/division-of-polynomials-in-python
+
+    A polynomial is represented by a list of its coefficients, eg
+    5*x**3 + 4*x**2 + 1 -> [1, 0, 4, 5]
+
+    Modified by PM 2Ring 2014.10.03
+    Modified by NickT to work with integers only
+    """
+    # Create normalized copies of the args
+    num = num[:]
+    normalize(num)
+    den = den[:]
+    normalize(den)
+
+    if len(num) >= len(den):
+        # Shift den towards right so it's the same degree as num
+        shiftlen = len(num) - len(den)
+        den = [0] * shiftlen + den
+    else:
+        return [0], num
+
+    quotient = []
+    divisor = den[-1]  # removed float
+    for i in range(shiftlen + 1):
+        # Get the next coefficient of the quotient.
+        mult = num[-1] // divisor  # truncating division
+        quotient = [mult] + quotient
+
+        # Subtract mult * den from num, but don't bother if mult == 0
+        # Note that when i==0, mult!=0; so quot is automatically normalized.
+        if mult != 0:
+            d = [mult * u for u in den]
+            num = [u - v for u, v in zip(num, d)]
+
+        num.pop()
+        den.pop(0)
+
+    normalize(num)
+    return quotient, num
--- tests/test_finite.py
+++ tests/test_finite.py
@@ -1,3 +1,6 @@
+import itertools
+import operator
+import random
 import re
 
 import pytest
@@ -5,8 +8,11 @@ import pytest
 import finite
 
 
+SMALL_FIELDS = [2, 3, 4, 5, 7, 8, 9, 11, 13, 16, 17, 19, 23, 25, 27]
+
+
 @pytest.mark.parametrize(
-    "n, ex_base, ex_pow",
+    "n, ex_prime, ex_pow",
     [
         (2, 2, 1),
         (3, 3, 1),
@@ -18,10 +24,18 @@ import finite
         (16, 2, 4),
     ],
 )
-def test_field_creation(n, ex_base, ex_pow):
+def test_field_creation(n, ex_prime, ex_pow):
     f = finite.Field(n)
-    assert f.base == ex_base
+    assert f.prime == ex_prime
     assert f.power == ex_pow
+
+
+def test_superscripts():
+    assert (
+        finite.int2sup("123")
+        == "¹²³"
+        == "\N{SUPERSCRIPT ONE}\N{SUPERSCRIPT TWO}\N{SUPERSCRIPT THREE}"
+    )
 
 
 @pytest.mark.parametrize(
@@ -29,10 +43,11 @@ def test_field_creation(n, ex_base, ex_pow):
     [
         (6, "2 * 3"),
         (10, "2 * 5"),
-        (5040, "2**4 * 3**2 * 5 * 7"),
+        (12, "2² * 3"),
+        (5040, "2⁴ * 3² * 5 * 7"),
     ],
 )
-def test_bad_fields(n, factorization):
+def test_invalid_fields(n, factorization):
     with pytest.raises(ValueError, match=re.escape(factorization)):
         finite.Field(n)
 
@@ -49,3 +64,156 @@ def test_element_generation():
     f = finite.Field(9)
     el = f.element(7)
     assert el.coeff == [1, 2]
+
+
+@pytest.mark.parametrize("n", SMALL_FIELDS)
+def test_element_roundtrip(n):
+    f = finite.Field(n)
+    for m in range(n):
+        el = f.element(m)
+        assert int(el) == m
+
+
+@pytest.mark.parametrize("n", SMALL_FIELDS)
+def test_characteristic(n):
+    f = finite.Field(n)
+    for m in range(n):
+        el = f.element(m)
+        assert sum(el for _ in range(f.prime)) == f.element(0)
+
+
+@pytest.mark.parametrize("n", SMALL_FIELDS)
+@pytest.mark.parametrize("func", [operator.add, operator.sub])
+def test_add_identity(n, func):
+    f = finite.Field(n)
+    additive_identity = f.element(0)
+    for m in range(n):
+        el = f.element(m)
+        assert func(el, additive_identity) == el
+
+
+
+@pytest.mark.parametrize("n", SMALL_FIELDS)
+def test_add_inverse(n):
+    f = finite.Field(n)
+    additive_identity = f.element(0)
+    for m in range(n):
+        el = f.element(m)
+        assert el + (-el) == additive_identity
+
+
+@pytest.mark.parametrize("func", [operator.add, operator.mul])
+@pytest.mark.parametrize("n", SMALL_FIELDS)
+def test_commutative(n, func):
+    f = finite.Field(n)
+    for na, nb in itertools.product(range(n), range(n)):
+        a = f.element(na)
+        b = f.element(nb)
+        assert func(a, b) == func(b, a), (a, b)
+
+
+@pytest.mark.parametrize("func", [operator.add, operator.mul])
+@pytest.mark.parametrize("n", SMALL_FIELDS)
+def test_associative(n, func):
+    f = finite.Field(n)
+    if n**3 > 1000:
+        rng = random.Random()
+        rng.seed(n)
+        def _gen():
+            yield rng.randrange(n), rng.randrange(n), rng.randrange(n)
+        gen = _gen()
+    else:
+        gen = itertools.product(range(n), range(n), range(n))
+
+    for na, nb, nc in gen:
+        print(na, nb, nc)
+        a = f.element(na)
+        b = f.element(nb)
+        c = f.element(nc)
+        assert func(func(a, b), c) == func(a, func(b, c)), (a, b, c)
+
+
+@pytest.mark.parametrize("n", SMALL_FIELDS)
+def test_sub(n):
+    f = finite.Field(n)
+    additive_identity = f.element(0)
+    for m in range(n):
+        el = f.element(m)
+        assert el - el == additive_identity
+
+
+@pytest.mark.parametrize("n", SMALL_FIELDS)
+def test_mul_identity(n):
+    f = finite.Field(n)
+    multiplicitive_identity = f.element(1)
+    for m in range(n):
+        el = f.element(m)
+        x = el * multiplicitive_identity
+        print(x)
+        print(el.unreduced_mul(multiplicitive_identity))
+        assert el * multiplicitive_identity == el
+
+
+@pytest.mark.xfail(reason="dunno...")
+def test_aes_mul():
+    f = finite.Field(2**8)
+    assert f.mod_poly == [1, 1, 0, 1, 1, 0, 0, 0, 1]
+
+    assert f.element(0x57) * f.element(0x01) == f.element(0x57)
+    assert f.element(0x57) * f.element(0x02) == f.element(0xAE)
+    assert f.element(0x57) * f.element(0x10) == f.element(0x07)
+    assert f.element(0x57) * f.element(0x13) == f.element(0xFE)
+
+
+@pytest.mark.parametrize("n", SMALL_FIELDS)
+def test_div_identity(n):
+    f = finite.Field(n)
+    multiplicitive_identity = f.element(1)
+    for m in range(n):
+        el = f.element(m)
+        quot = el / multiplicitive_identity
+
+        # repad quotient to same number of coefficients
+        quot = quot + [0] * (f.power - len(quot))
+
+        assert f.element(quot) == el
+
+
+def evaluate_poly(poly, x):
+    return sum(coeff * x ** k for k, coeff in enumerate(poly))
+
+
+@pytest.mark.parametrize(
+    "prime, power",
+    itertools.chain(
+        *(
+            [(prime, power) for power in polys]
+            for prime, polys in finite.POLY_MIN_WEIGHT.items()
+        )
+    ),
+)
+def test_polynomials_irreducible(prime, power):
+    poly = finite.POLY_MIN_WEIGHT[prime][power]
+    for n in range(-(prime - 1), prime):
+        assert evaluate_poly(poly, n) != 0
+
+
+def test_unreduced_multiplication():
+    f = finite.Field(27)
+    assert f.element([2, 1, 2]).unreduced_mul(f.element([2, 1, 2])) == [1, 1, 0, 1, 1]
+
+
+def test_poly_divmod():
+    num = [1, 5, 10, 10, 5, 1]
+    den = [1, 2, 1]
+    finite.poly_divmod(num, den)
+
+    num = [5, 16, 10, 22, 7, 11, 1, 3]
+    den = [1, 2, 1, 3]
+
+    quot = [5, 1, 3, 0, 1]
+    rem = [0, 5]
+
+    q, r = finite.poly_divmod(num, den)
+    assert quot == q
+    assert rem == r
